7079424c84b6435822790a73775c40464877d168
usmailaabdoul <ismaelabdul77@gmail.com>
2020-05-22 08:32:01 +0100

    Requested changes

--- Week1/assignments/usmaila-abdoul/Staircase.js
+++ Week1/assignments/usmaila-abdoul/Staircase.js
@@ -1,16 +1,16 @@
 const staireCase = (n) => {
 	const Arr = [];
   
-  for (let i =0; i <= n; i++) {
+  for (let i =0; i <= n - 1; i++) {
   	Arr.push(' ');
   }
   
-  for (let i = 0; i<= n; i++) {
+  for (let i = 0; i<= n - 1; i++) {
   	Arr.shift();
     Arr.push('#');
-    var arr = Arr.join('')
+    let arr = Arr.join('')
     console.log(arr)
   }
 }
 
-staireCase(5); //for n = 5 output will be 
+staireCase(4); //for n = 5 output will be 
