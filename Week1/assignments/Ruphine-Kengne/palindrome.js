f10842f3c581ea09eecaedd2a2783bbcd47ef924
kengne ruphine <ruphinekengne@gmail.com>
2020-05-22 13:43:27 +0100

    Providing another method to the palindrome issue

--- Week1/assignments/Ruphine-Kengne/palindrome.js
+++ Week1/assignments/Ruphine-Kengne/palindrome.js
@@ -4,16 +4,33 @@
 //and then test the created string against what was originally received
 
 const isPalindrome = str => {
-    if (str.toLowerCase() === str.toLowerCase().split('').reverse().join('')) {
-        return true;
-    } else {
-        return false;
-    }
-
+    return (str.toLowerCase() === str.toLowerCase().split('').reverse().join('')) 
 }
 
-const str = 'dad';
+const str = 'moom';
 console.log('test', isPalindrome(str));
 
 const str2 = 'daddy';
 console.log('test2', isPalindrome(str2));
+
+//Using another method to check for palindrome
+/** We will use a for loop to iterate through the characters
+ * For each character at position i, we compare with the character at position i from the end
+ * If any of these does not equal, we can reject the input as being a palindrome and we break out of the loop and return a false
+ * We also only need to iterate through the half of the string, since going through the whole string 
+ * would be comparing every character twice
+ */
+
+const checkPalindrome = str => {
+    const lowerString = str.toLowerCase();
+
+    for (let i = 0; i < lowerString.length / 2; i++){
+        if (lowerString[i] !== lowerString[lowerString.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+ 
+console.log('second check', checkPalindrome(str));
+console.log('check', checkPalindrome(str2));
